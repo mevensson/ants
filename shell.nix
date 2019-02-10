5c8826d20cd056383b1848e2b1c73b67f5248240
Mattias Evensson <mattias@evensson.eu>
2019-02-10 19:52:14 +0100

    Update VSCode extensions and add Git Lens

--- shell.nix
+++ shell.nix
@@ -7,8 +7,14 @@ let
         {
           name = "githistory";
           publisher = "donjayamanne";
-          version = "0.4.4";
+          version = "0.4.6";
           sha256 = "12vhs0jrzqh1gq38qfq0h3pfpnan01sx4a4ik14hdby0558han9j";
+        }
+        {
+          name = "gitlens";
+          publisher = "eamodio";
+          version = "9.5.0";
+          sha256 = "1gzw65p3z1zkbq165xmki4cp2as3y8smyr8my9bv7j5hkz7p8xvs";
         }
         {
           name = "rust";
