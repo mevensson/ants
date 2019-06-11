79256b8c3020636d0e83c802ad6a6408b51612dc
Mattias Evensson <mattias@evensson.eu>
2019-06-11 23:41:39 +0200

    Add sketch for DQN strategy

--- src/strategies/dqn/mod.rs
+++ src/strategies/dqn/mod.rs
@@ -3,16 +3,27 @@ mod test;
 
 use super::{Ant, Command, Food, Strategy};
 
-pub struct DqnStrategy {}
+pub struct DqnStrategy {
+    dqn: Dqn;
+}
 
 impl DqnStrategy {
-    pub fn new() -> Self {
-        DqnStrategy {}
+    pub fn new(dqn: Dqn) -> Self {
+        DqnStrategy {
+            dqn
+        }
     }
 }
 
 impl Strategy for DqnStrategy {
-    fn run(&mut self, _ants: &Vec<Ant>, _food: &Vec<Food>) -> Vec<Command> {
-        Vec::new()
+    fn run(&mut self, ants: &Vec<Ant>, food: &Vec<Food>) -> Vec<Command> {
+        let mut result = Vec::new();
+        for ant in ants {
+            let input_tensor = convert_input(ant, ants, food);
+            let output_tensor = self.dqn.run(input_tensor);
+            let command = convert_output(output_tensor);
+            result.push(command);
+        }
+        result
     }
 }
